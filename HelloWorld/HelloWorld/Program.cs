327dd420b2b40547f2c3f33bf19ef5b3d8273eb0
Horea Oros <horea.oros@gmail.com>
2019-02-26 12:51:47 +0200

    Add Hello World POO project

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            World w1 = new World("ET1");
+            w1.SayHello();
+
+            World w2 = new World("ET2");
+            w2.SayHello();
+
+            World w3 = new World();
+            w3.SayHello();
+            
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/World.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HelloWorld
+{
+    internal class World
+    {
+        string name;
+        public void SayHello()
+        {
+            Console.WriteLine("Hello from {0}", name);
+        }
+        /// <summary>
+        /// Metoda constructor - initializeaza instantele clasei
+        /// </summary>
+        /// <param name="name">World name</param>
+        public World(string name1)
+        {
+            name = name1;
+        }
+        /// <summary>
+        /// Metoda constructor - implicit
+        /// </summary>
+        public World()
+        {
+            this.name = "<none/>";
+        }
+    }
+}
