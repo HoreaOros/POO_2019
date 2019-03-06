9afcb23c4a84c4ce634eb969ef7473e1250d0551
Horea Oros <horea.oros@gmail.com>
2019-03-06 10:47:31 +0200

    Add NumarRational

---
+++ NumarRational/NumarRational/Fractie.cs
@@ -0,0 +1,90 @@
+﻿using System;
+namespace NumarRational
+{
+    public class Fractie
+    {
+        private int numarator, numitor;
+        
+
+        public Fractie()
+        {
+            numarator = 0;
+            numitor = 1;
+        }
+
+        public Fractie(string v)
+        {
+            string[] tokens = v.Split('/');
+
+            int numarator, numitor;
+
+            if (tokens.Length != 2)
+            {
+                this.numarator = 0;
+                this.numitor = 1;
+            }
+            else
+            {
+
+                bool r1 = int.TryParse(tokens[0], out numarator);
+                bool r2 = int.TryParse(tokens[1], out numitor);
+
+                if (r1 && r2)
+                {
+                    int d;
+
+                    d = Util.gcd(numarator, numitor);
+                    this.numarator = numarator / d;
+                    this.numitor = numitor / d;
+                }
+                else
+                {
+                    this.numarator = 0;
+                    this.numitor = 1;
+                }
+            }
+        }
+
+        public Fractie Add(Fractie right)
+        {
+            int r_numarator, r_numitor;
+
+            r_numitor = numitor * right.numitor;
+            r_numarator = numarator * right.numitor + numitor * right.numarator;
+            return new Fractie(r_numarator, r_numitor);
+        }
+
+        public Fractie(int numarator, int numitor) 
+        {
+            int d;
+
+            d = Util.gcd(numarator, numitor);
+            this.numarator = numarator / d;
+            this.numitor = numitor / d;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(numarator + " / " + numitor);
+        }
+    }
+
+    internal class Util
+    {
+        private Util()
+        {
+
+        }
+        public static int gcd(int a, int b)
+        {
+            int r;
+            while (b != 0)
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            return a;
+        }
+    }
+}
---
+++ NumarRational/NumarRational/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumarRational
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fractie f1 = new Fractie();
+            Fractie f2 = new Fractie(3, 9);
+
+            f1.Print();
+            f2.Print();
+
+            Fractie f3 = new Fractie("  4 / 8  ");
+            f3.Print();
+
+            Fractie f4;
+
+            f4 = f2.Add(f3);
+            f4.Print();
+
+            /** TODO
+            f4 = f2.Multiply(f3);
+            f4.Print();
+
+            f4 = f2.Subtract(f3);
+            f4.Print();
+
+            f4 = f2.Divide(f3);
+            f4.Print();
+            */
+
+        }
+    }
+}
